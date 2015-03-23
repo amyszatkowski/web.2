af39286ac437b9bc6d332b8383e2ad8f48e25265
Amy Szatkowski <ajszatkowski@massart.edu>
2015-03-23 15:26:07 -0400

    bug fixed
    fixed in class

--- MassPaint/draw.js
+++ MassPaint/draw.js
@@ -20,7 +20,9 @@ $(".tool").removeClass("selected");
 $("#button-pen").on("click",function(){
 	$(".tool").removeClass("selected");
 	$(this).addClass("selected");
-
+    tool = "pen"
+    lineWidth(width)
+    lineColor(color)
 });
 
 $("#button-eraser").on("click",function() {
@@ -1116,249 +1118,30 @@ function flowerSmall(){
 	forward(3)
 	rotate(15)
 
-
-
-
-
-
-
-
-
-
-
 }
-
-///END STAMPS///
-
 
 
 ///START COLORS////
 
-$("#button-black").on("click", function(){
+
+$(".color").on("click", function() {
+	var col = $(this).data("color");
+	lineColor(col);
 	$(".color").removeClass("selected");
 	$(this).addClass("selected");
-
-	lineColor("black")
-});
-
-$("#button-brightBlue").on("click", function(){
-	$(".color").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineColor("#33CCFF")
-});
-
-$("#button-medBlue").on("click", function(){
-	$(".color").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineColor("#99CCFF")
-});
-
-$("#button-darkBlue").on("click", function(){
-	$(".color").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineColor("#0093B8")
-});
-
-$("#button-oceanBlue").on("click", function(){
-	$(".color").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineColor("#CCFFFF")
-});
-$("#button-trueBlue").on("click", function(){
-	$(".color").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineColor("#0099FF")
-});
-
-$("#button-orange").on("click", function(){
-	$(".color").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineColor("#FFB42F")
-});
-
-$("#button-darkOrange").on("click", function(){
-	$(".color").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineColor("#CC5600")
-});
-
-$("#button-green").on("click", function(){
-	$(".color").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineColor("#0AF54B")
 });
 
 
-$("#button-darkGreen").on("click", function(){
-	$(".color").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineColor("#4A8C00")
-});
-
-$("#button-yellow").on("click", function(){
-	$(".color").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineColor("#F3EF00")
-});
-
-$("#button-darkYellow").on("click", function(){
-	$(".color").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineColor("#CBC700")
-});
-
-$("#button-spongeYellow").on("click", function(){
-	$(".color").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineColor("#FFFF00")
-});
-
-$("#button-brown").on("click", function(){
-	$(".color").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineColor("#996600")
-});
-
-$("#button-red").on("click", function(){
-	$(".color").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineColor("#E62E00")
-});
-
-$("#button-purple").on("click", function(){
-	$(".color").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineColor("#CC00CC")
-});
-
-$("#button-pink").on("click", function(){
-	$(".color").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineColor("#FF92EB")
-});
-
-$("#button-sand").on("click", function(){
-	$(".color").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineColor("#FFFFCC")
-});
-
-
-$("#button-greyGreen").on("click", function(){
-	$(".color").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineColor("#006666")
-});
-
-$("#button-seaweed").on("click", function(){
-	$(".color").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineColor("#849822")
-});
-
-$("#button-sandDark").on("click", function(){
-	$(".color").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineColor("#E6E6B8")
-});
-
-$("#button-white").on("click", function(){
-	$(".color").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineColor("white")
-});
-
-$("#button-gray").on("click", function(){
-	$(".color").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineColor("#B3BAB3")
-});
-
-$("#button-lightGray").on("click", function(){
-	$(".color").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineColor("#DEE1DE")
-});
 
 ////LINE WIDTH/////
 
 
-$("#button-lineLight").on("click", function(){
+$(".line").on("click", function(){
+	var width = $(this).data("width");
+	lineWidth(width);
 	$(".line").removeClass("selected");
 	$(this).addClass("selected");
-
-	lineWidth(1)
 });
-
-$("#button-lineReg").on("click", function(){
-	$(".line").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineWidth(5)
-});
-
-$("#button-lineMed").on("click", function(){
-	$(".line").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineWidth(10)
-});
-
-$("#button-lineBold").on("click", function(){
-	$(".line").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineWidth(25)
-});
-
-$("#button-lineBlack").on("click", function(){
-	$(".line").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineWidth(50)
-});
-
-$("#button-lineUltra").on("click", function(){
-	$(".line").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineWidth(80)
-});
-
-$("#button-lineSuper").on("click", function(){
-	$(".line").removeClass("selected");
-	$(this).addClass("selected");
-
-	lineWidth(100)
-});
-
-
-///END COLORS///
-
-
 
 
 
@@ -1368,23 +1151,17 @@ $("#button-lineSuper").on("click", function(){
 var drawing = false;
 var tool = "pen";
 var color = "black";
+var width = 2
 
-$(document).on("mousedown",function(e) {
-  e.preventDefault();
-  moveTo(e.pageX,e.pageY);
-  if(tool == "pen") { 
-    drawing = true;
-  }
-});
 
-$(document).on("mousemove",function(e) {
+$("canvas").on("mousemove",function(e) {
   e.preventDefault();
   if(drawing) {
     lineTo(e.pageX,e.pageY);
   }
 });
 
-$(document).on("mouseup",function(e) {
+$("canvas").on("mouseup",function(e) {
   e.preventDefault();
   drawing = false;
 });
@@ -1393,45 +1170,27 @@ $(document).on("mouseup",function(e) {
 
 ///STAMPS///
 
-$(document).on("mousedown",function(e) {
+$("canvas").on("mousedown",function(e) {
   e.preventDefault();
   moveTo(e.pageX,e.pageY);
-  if(tool == "pen" || tool =="eraser" || tool == "flowerSmall" || tool == "bubbleSmall"  || tool == "flower") { 
+  if(tool == "pen" || tool =="eraser") { 
     drawing = true;
+  
   } else if(tool == "bubble") {
     bubble();
-  }
-});
-
-$(document).on("mousedown",function(e) {
-  e.preventDefault();
-  moveTo(e.pageX,e.pageY);
-  if(tool == "pen" || tool =="eraser" || tool == "bubble" || tool == "flowerSmall" || tool == "flower") { 
-    drawing = true;
-  } else if(tool == "bubbleSmall") {
+  
+  }else if(tool == "bubbleSmall") {
     bubbleSmall();
-  }
-});
-
-$(document).on("mousedown",function(e) {
-  e.preventDefault();
-  moveTo(e.pageX,e.pageY);
-  if(tool == "pen" || tool =="eraser" || tool == "bubble" || tool == "bubbleSmall"  || tool == "flowerSmall") { 
-    drawing = true;
-  } else if(tool == "flower") {
+ 
+   } else if(tool == "flower") {
     flower();
-  }
-});
-
-$(document).on("mousedown",function(e) {
-  e.preventDefault();
-  moveTo(e.pageX,e.pageY);
-  if(tool == "pen" || tool =="eraser" || tool == "bubble" || tool == "bubbleSmall" || tool == "flower") { 
-    drawing = true;
+  
   } else if(tool == "flowerSmall") {
     flowerSmall();
   }
 });
+
+
 
 
 
