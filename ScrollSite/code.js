2c8556e6d58479da1af2bbebbeaae7ecab6bd988
Amy Szatkowski <ajszatkowski@massart.edu>
2015-04-19 14:35:49 -0400

    getting menu to work!
    woohoo!

--- ScrollSite/code.js
+++ ScrollSite/code.js
@@ -1,6 +1,8 @@
 $(".scrolling").scrollFlight();
 
 
+/// Image fade in ///
+
 $("#imageRight").on("arrived",function(e){
 	$(this).addClass("on-page");
 	});
@@ -56,6 +58,40 @@ $(".li").removeClass("selected");
 $(this).addClass("selected");
 })
 
+
+///Dot Menu ////
+
+
+
+$(".scroller").scrollFlight();
+
+$(".section").on("arriving rearriving",function() {
+  $(this).find(".title").addClass("arrived");
+});
+
+
+$(".section").on("arriving rearriving",function() {
+  // Grab find the href of the menu it to go to
+  var href='#' + this.id;
+
+  // Unselect the current menu item
+  $(".menu a.selected").removeClass("selected");
+
+  // Add a class of selected to the menu item with this page as an anchor
+  $(".menu").find("a[href='" + href + "']").addClass("selected");
+});
+
+// Optional support for smooth scroll
+$(".menu a").on("click",function(e) {
+  // prevent the browser from jumping
+  e.preventDefault();
+
+  // Get the position of the page 
+  var top = $($(this).attr('href')).position().top;
+
+  // Animate to the body to scroll to the proper place
+  $('html, body').stop().animate({ scrollTop: top });
+});
 
 
 
